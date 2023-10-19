c198fefbef70bbbdbc7e9d5e4b1f8aaa19131d39
Mohsen! <abdelmohsen@outlook.ie>
2023-10-19 12:01:21 +0100

    project clean up

---
+++ app/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <h1>Tech Mahindra</h1>
+    </div>
+  );
+};
+
+export default Header;
